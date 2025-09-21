825695fc730fcc3471e9da0c98b611ab15ed543f
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-09-21 16:54:02 +0200

    update config

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const config = {
 	db: {
 		host: process.env.DB_HOST || 'localhost',
-		port: parseInt(process.env.DB_PORT || '3306', 10),
+		port: parseInt(process.env.DB_PORT || '5432', 10),
 		username: process.env.DB_USER,
 		password: process.env.DB_PASSWORD,
 		database: process.env.DB_NAME || 'test_db',
