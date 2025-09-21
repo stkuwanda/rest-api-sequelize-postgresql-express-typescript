9c424cf72d6f9a9effed6ff3b2dfec5431eb7f20
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-09-21 17:23:08 +0200

    updte config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,3 @@
-// const config = {
-// 	db: {
-// 		host: process.env.DB_HOST || 'localhost',
-// 		port: parseInt(process.env.DB_PORT || '5432', 10),
-// 		username: process.env.DB_USER,
-// 		password: process.env.DB_PASSWORD,
-// 		database: process.env.DB_NAME || 'test_db',
-// 	},
-// };
-
 const config = {
 	host: process.env.DB_HOST || 'localhost',
 	port: parseInt(process.env.DB_PORT || '5432', 10),
